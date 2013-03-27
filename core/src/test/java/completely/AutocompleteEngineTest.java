70fa74ffa9c920a8a14eedba194126cf59761138
Miguel Fonseca <contact@miguelfonseca.com>
2013-03-27 21:22:07 +0100

    replace Record with TestRecord

--- core/src/test/java/completely/AutocompleteEngineTest.java
+++ core/src/test/java/completely/AutocompleteEngineTest.java
@@ -1,6 +1,6 @@
 package completely;
 
-import completely.data.Record;
+import completely.data.Indexable;
 import completely.text.index.HashMultiMap;
 import java.util.List;
 
@@ -10,22 +10,22 @@ import static org.junit.Assert.assertEquals;
 
 public class AutocompleteEngineTest
 {
-    private AutocompleteEngine<Record> engine;
+    private AutocompleteEngine<TestRecord> engine;
 
     public AutocompleteEngineTest()
     {
-        engine = new AutocompleteEngine.Builder<Record>()
-            .setIndex(new HashMultiMap<Record>())
+        engine = new AutocompleteEngine.Builder<TestRecord>()
+            .setIndex(new HashMultiMap<TestRecord>())
             .build();
     }
 
     @Test
     public void testSearchSort()
     {
-        engine.add(new Record("a", 0));
-        engine.add(new Record("a", 1));
-        engine.add(new Record("a", 2));
-        List<Record> result = engine.search("a");
+        engine.add(new TestRecord("a", 0));
+        engine.add(new TestRecord("a", 1));
+        engine.add(new TestRecord("a", 2));
+        List<TestRecord> result = engine.search("a");
         assertEquals(3, result.size());
         assertEquals(2, result.get(0).getScore(), 0);
         assertEquals(1, result.get(1).getScore(), 0);
@@ -35,12 +35,36 @@ public class AutocompleteEngineTest
     @Test
     public void testSearchLimit()
     {
-        engine.add(new Record("a", 0));
-        engine.add(new Record("a", 1));
-        engine.add(new Record("a", 2));
-        List<Record> result = engine.search("a", 2);
+        engine.add(new TestRecord("a", 0));
+        engine.add(new TestRecord("a", 1));
+        engine.add(new TestRecord("a", 2));
+        List<TestRecord> result = engine.search("a", 2);
         assertEquals(2, result.size());
         assertEquals(2, result.get(0).getScore(), 0);
         assertEquals(1, result.get(1).getScore(), 0);
+    }
+
+    private static class TestRecord implements Indexable
+    {
+        private final String text;
+        private final double score;
+
+        TestRecord(String text, double score)
+        {
+            this.text = text;
+            this.score = score;
+        }
+
+        @Override
+        public String getText()
+        {
+            return text;
+        }
+
+        @Override
+        public double getScore()
+        {
+            return score;
+        }
     }
 }
