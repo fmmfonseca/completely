31f74c10a9cf29c09408bb53907b2b8b91a9af52
Miguel Fonseca <contact@miguelfonseca.com>
2015-01-03 19:32:56 +0100

    add Precondition

---
+++ core/src/main/java/completely/common/Precondition.java
@@ -0,0 +1,139 @@
+package completely.common;
+
+/**
+ * Condition that must be true prior to a routine's execution.
+ */
+public final class Precondition
+{
+    private Precondition() { };
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws IllegalArgumentException if {@code expression} is false;
+     */
+    public static void checkArgument(boolean expression)
+    {
+        if (!expression)
+        {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws IllegalArgumentException if {@code expression} is false;
+     */
+    public static void checkArgument(boolean expression, String message)
+    {
+        if (!expression)
+        {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws ArithmeticException if {@code expression} is false;
+     */
+    public static void checkArithmetic(boolean expression)
+    {
+        if (!expression)
+        {
+            throw new ArithmeticException();
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws ArithmeticException if {@code expression} is false;
+     */
+    public static void checkArithmetic(boolean expression, String message)
+    {
+        if (!expression)
+        {
+            throw new ArithmeticException(message);
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws IndexOutOfBoundsException if {@code expression} is false;
+     */
+    public static void checkIndex(boolean expression)
+    {
+        if (!expression)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws IndexOutOfBoundsException if {@code expression} is false;
+     */
+    public static void checkIndex(boolean expression, String message)
+    {
+        if (!expression)
+        {
+            throw new IndexOutOfBoundsException(message);
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws NullPointerException if {@code expression} is false;
+     */
+    public static void checkPointer(boolean expression)
+    {
+        if (!expression)
+        {
+            throw new NullPointerException();
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws NullPointerException if {@code expression} is false;
+     */
+    public static void checkPointer(boolean expression, String message)
+    {
+        if (!expression)
+        {
+            throw new NullPointerException(message);
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws IllegalStateException if {@code expression} is false;
+     */
+    public static void checkState(boolean expression)
+    {
+        if (!expression)
+        {
+            throw new IllegalStateException();
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression.
+     *
+     * @throws IllegalStateException if {@code expression} is false;
+     */
+    public static void checkState(boolean expression, String message)
+    {
+        if (!expression)
+        {
+            throw new IllegalStateException(message);
+        }
+    }
+}
