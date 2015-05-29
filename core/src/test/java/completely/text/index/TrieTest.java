066113f826ce67a104cc23a12041123b93a5cdbf
Miguel Fonseca <contact@miguelfonseca.com>
2015-05-29 08:28:25 +0200

    rename IndexTest to AbstractIndexTest

--- core/src/test/java/completely/text/index/TrieTest.java
+++ core/src/test/java/completely/text/index/TrieTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class TrieTest extends IndexTest<Trie<Object>>
+public class TrieTest extends AbstractIndexTest<Trie<Object>>
 {
     public TrieTest()
     {
